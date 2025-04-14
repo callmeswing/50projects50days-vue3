8f81b27db6b0ccdca300c71d44d45cdb88be3350
callmeswing <swingwingingngg@gmail.com>
2025-04-14 17:50:56 +0800

    ...

---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>This is Home</p>
+  </div>
+</template>
+<script setup lang="ts"></script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // 路径别名: 1.此处 2.tsconfig.json(如果是ts项目)
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@api": "/src/api",
+      "@cp": "/src/components",
+      "@views": "/src/views",
+    },
+  },
+});
