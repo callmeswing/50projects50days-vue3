f8d8aff9f97fe6a9e718250bd2e70862f8a12702
callmeswing <swingwingingngg@gmail.com>
2025-04-28 12:36:50 +0800

    finish day08 pro

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "./styles/global.scss";
 import "./mock";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(Antd).mount("#app");
--- src/views/projects/Day08/FormWave.vue
+++ src/views/projects/Day08/FormWave.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="container">
     <div class="login-card">
-      <h1 class="title">Login 😸</h1>
+      <h1 class="title">Sign up 🤗</h1>
 
       <form action="">
         <div class="input-container">
+          <!-- 关于邮箱长度 根据协议 邮箱最长也就254 -->
           <input
-            type="email"
+            type="text"
+            size="254"
             class="email"
             id="email"
-            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-            required
+            v-model="email"
+            @blur="validateEmial"
+            :class="[emailHang ? 'hang' : '', emailError ? 'error' : '']"
+            @focus="
+              () => {
+                emailHang = true;
+                emailError = false;
+              }
+            "
           />
           <label for="email" class="email-label">
             <span style="transition-delay: 0ms">E</span>
@@ -23,11 +32,18 @@
 
         <div class="input-container">
           <input
-            type="text"
+            type="password"
             class="password"
             id="password"
-            pattern="[A-Za-z0-9]+"
-            required
+            v-model="password"
+            @blur="validatePssword"
+            :class="[passwordHang ? 'hang' : '', passwordErorr ? 'error' : '']"
+            @focus="
+              () => {
+                passwordHang = true;
+                passwordErorr = false;
+              }
+            "
           />
           <label for="password" class="password-label">
             <span style="transition-delay: 0ms">P</span>
@@ -39,21 +55,122 @@
             <span style="transition-delay: 240ms">r</span>
             <span style="transition-delay: 280ms">d</span>
           </label>
+
+          <!-- 这里想做淡入淡出的效果 但是发现hidden没办法实现：hidden本质是通过display:none 而display是无法被transition影响的 -->
+          <!-- 用 opacity、transform、visibility 等配合 transition -->
+          <span
+            ref="shakeReminder"
+            class="password-reminder"
+            style="transition-delay: 160ms"
+            >8-16 digits, capital and lower-case letter, numbers and those (
+            !@#*+\-=._ ) are allowd</span
+          >
         </div>
 
-        <button type="button" class="btn">Login</button><br />
+        <button
+          type="button"
+          class="btn"
+          :disabled="!btnActive"
+          @click="signUp"
+        >
+          Login</button
+        ><br />
 
         <div class="to-register">
-          <span
-            >Don't have an account? <a href="#">&nbsp;&nbsp;Sign up</a></span
-          >
+          <span>Already have account? <a href="#">&nbsp;&nbsp;Sign in</a></span>
         </div>
       </form>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { notification } from "ant-design-vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+// 表单数据
+const email = ref<string>("");
+const password = ref<string>("");
+
+// 登录按钮控制
+const btnActive = ref(false);
+
+// input label 状态
+const emailHang = ref(false);
+const emailError = ref(false);
+const passwordHang = ref(false);
+const passwordErorr = ref(false);
+
+// 校验规则
+const emailRegex =
+  /^[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\.[A-Za-z]{2,}$/;
+const passwordRegex = /^(?=[\w!@#*+\-=.]{8,16}$)[\w!@#*+\-=.]+$/;
+
+function validateEmial() {
+  btnActive.value = false;
+  email.value = email.value.trim();
+  if (email.value === "") {
+    // 默认值，不校验，字符位置还原，不进行提示
+    emailHang.value = false;
+  } else {
+    // 校验，通过与否样式都不还原，不通过需要提示，并将字体颜色修改，再次input的时候修正
+    if (emailRegex.test(email.value)) {
+      if (passwordRegex.test(password.value)) {
+        btnActive.value = true;
+      }
+    } else {
+      emailError.value = true;
+      notification.warn({
+        message: "Invalid email",
+        description: "Please enter a valid email address",
+      });
+    }
+  }
+}
+
+function validatePssword() {
+  btnActive.value = false;
+  password.value = password.value.trim();
+  if (password.value != "") {
+    // 1.不为空，进行校验
+    if (passwordRegex.test(password.value)) {
+      // 1.1正确 校验一次email 通过放行按钮
+      if (emailRegex.test(email.value)) {
+        btnActive.value = true;
+      }
+    } else {
+      // 1.2错误 error样式，动画以及提示
+      passwordErorr.value = true;
+      triggerShake();
+      notification.warn({
+        message: "Invalid password",
+        description: "Please follow the password requirements",
+      });
+    }
+  } else {
+    // 2.为空 取消hang
+    passwordHang.value = false;
+  }
+}
+
+// 触发动画
+const shakeReminder = ref<HTMLElement | null>(null);
+const triggerShake = () => {
+  if (shakeReminder.value) {
+    shakeReminder.value.classList.remove("shake"); // 先移除，确保能重新触发动画
+    void shakeReminder.value.offsetWidth; // 强制重绘，重新触发 animation
+    shakeReminder.value.classList.add("shake");
+  }
+};
+
+// Success
+function signUp() {
+  router.push("/");
+}
+</script>
 
 <style lang="scss" scoped>
 @use "./form-wave.scss";
